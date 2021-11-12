d7a0702d9f195e4fc549b0255a0e5dea75e2a78b
Davi Barreira <davisbarreira@gmail.com>
2021-11-12 17:55:59 -0300

    :white_check_mark: Initial testing.

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,6 +1,37 @@
 using EmpiricalMeasure
+using ArraysOfArrays
+using LinearAlgebra
+using Random
 using Test
 
 @testset "EmpiricalMeasure.jl" begin
-    # Write your tests here.
+
+    Random.seed!(7)
+    n = 4
+    m = 2
+    A = nestedview(rand(n, m)')
+    p = normalize!(rand(n), 1)
+
+    μ = @inferred(MvDiscreteNonParametric{Float64,Float64,typeof(A),typeof(p)}(A, p))
+
+    @test support(μ) == A
+    @test length(μ) == 2
+    @test size(μ) == size(flatview(A)')
+    @test probs(μ)   == p
+
+    μ = @inferred(MvDiscreteNonParametric(A,p))
+    @test support(μ) == A
+    @test length(μ) == 2
+    @test size(μ) == size(flatview(A)')
+
+    μ = @inferred(MvDiscreteNonParametric(A))
+    @test support(μ) == A
+    @test length(μ) == 2
+    @test size(μ) == size(flatview(A)')
+    @test probs(μ)   == fill(1/n, n)
+
+    # Matrix
+    M = rand(10,2)
+    # μ = @inferred(MvDiscreteNonParametric(M))
+
 end
