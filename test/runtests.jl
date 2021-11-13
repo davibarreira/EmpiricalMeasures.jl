3602bbd1f47b62c92758e3e5b8c9ad3ce8b6b73f
Davi Barreira <davisbarreira@gmail.com>
2021-11-12 22:41:51 -0300

    :white_check_mark: Added test for variance.

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,5 +1,6 @@
 using EmpiricalMeasure
 using ArraysOfArrays
+using StatsBase
 using LinearAlgebra
 using Random
 using Test
@@ -19,7 +20,7 @@ using Test
     @test size(μ) == size(flatview(A)')
     @test probs(μ)   == p
 
-    μ = @inferred(MvDiscreteNonParametric(A,p))
+    μ = @inferred(MvDiscreteNonParametric(A, p))
     @test support(μ) == A
     @test length(μ) == 2
     @test size(μ) == size(flatview(A)')
@@ -28,10 +29,27 @@ using Test
     @test support(μ) == A
     @test length(μ) == 2
     @test size(μ) == size(flatview(A)')
-    @test probs(μ)   == fill(1/n, n)
+    @test probs(μ)   == fill(1 / n, n)
 
     # Matrix
-    M = rand(10,2)
+    M = rand(10, 2)
     μ = @inferred(MvDiscreteNonParametric(M))
 
+
+    @testset "Functionalities" begin
+        
+        p = ([3 / 5, 1 / 5, 1 / 5])
+        A = [[1,0],[1,1],[0,1]]
+        A = ArrayOfSimilarArrays{Int64}(A)
+        μ = MvDiscreteNonParametric(A, p)
+
+        v = flatview(μ.support)'[:,1]
+        m = mean(μ)[1]
+        @test sum(abs2.(v .- m), Weights(μ.p)) ≈ var(μ)[1]
+        println(var(μ))
+
+        v = flatview(μ.support)'[:,2]
+        m = mean(μ)[2]
+        @test sum(abs2.(v .- m), Weights(μ.p)) ≈ var(μ)[2]
+    end
 end
